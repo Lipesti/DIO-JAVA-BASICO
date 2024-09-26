147080664d9690f717cae8e6a4b7eae30726ec55
Felipe Santos <lipes.santos02@gmail.com>
2024-09-25 21:49:51 -0300

    Tipos de variaveis

--- tipo-variaveis-simples/src/TipoVariavel.java
+++ tipo-variaveis-simples/src/TipoVariavel.java
@@ -1,5 +1,30 @@
 public class TipoVariavel {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos de variaveis começando com Double 
+        double salarioMinimo = 2500;
+
+        //Byte
+        byte idade = 30;
+        
+        //Short
+        short numeroDePessoas = 10000;
+        
+        //Integer
+        int numeroDeFuncionarios = 1000;
+        
+        //Long
+        long numeroDeClientes = 1000000000;
+        
+        //Float
+        float precoDeComida = 10.5f;
+        
+        //Boolean
+        boolean usuarioLogado = true;
+        
+        //Char
+        char letra = 'a';
+        
+        //String
+        String nomeDoUsuario = "Juliana";
     }
 }
