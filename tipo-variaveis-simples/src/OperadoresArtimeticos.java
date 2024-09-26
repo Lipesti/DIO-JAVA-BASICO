46153ef3049c4707dfb572ba7aa8cae1db5b74e0
Felipe Santos <lipes.santos02@gmail.com>
2024-09-25 22:04:29 -0300

    Operadores Artimeticos

---
+++ tipo-variaveis-simples/src/OperadoresArtimeticos.java
@@ -0,0 +1,11 @@
+public class OperadoresArtimeticos {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero = -numero;
+
+        System.out.println("Valor de numero: " + numero); // Saída: Valor de numero: -5
+
+        numero = numero * -1; // Convertendo o numero para positivo
+        System.out.println("Valor de numero: " + numero); // Saída: Valor de numero: 5
+}
+}
