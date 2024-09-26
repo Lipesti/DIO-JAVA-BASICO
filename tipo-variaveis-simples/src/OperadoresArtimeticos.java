9e48d6b69a92d8ecfbac72655d8f134801f23413
Felipe Santos <lipes.santos02@gmail.com>
2024-09-25 22:18:56 -0300

    Incremento e decremento

--- tipo-variaveis-simples/src/OperadoresArtimeticos.java
+++ tipo-variaveis-simples/src/OperadoresArtimeticos.java
@@ -6,6 +6,17 @@ public class OperadoresArtimeticos {
         System.out.println("Valor de numero: " + numero); // Saída: Valor de numero: -5
 
         numero = numero * -1; // Convertendo o numero para positivo
-        System.out.println("Valor de numero: " + numero); // Saída: Valor de numero: 5
+        System.out.println("Valor de numero: " + numero + "\n"); // Saída: Valor de numero: 5
+
+        //INCREMENTO DE NUMEROS 
+        int number = 6;
+        System.out.println("Valor original do numero " + number); // Valor original do numero
+
+        number++; // INCREMENTO
+        System.out.println("Incremento " + number); 
+        
+        number = 6; // Resetando a variavel
+        number--; // Decremento de number 
+        System.out.println("Decremento " + number);
 }
 }
