a38f363b92cfb4a674915a2fb86d94c640862c44
Felipe Santos <lipes.santos02@gmail.com>
2024-09-28 20:27:16 -0300

    Meu projeto conta banco terminal

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+// Classe que simula um terminal de caixa eletrônico para contas bancárias
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+
+    }
+}
