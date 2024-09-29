4b07e89914b7fd5b251181e85b866030f4cd7be5
Felipe Santos <lipes.santos02@gmail.com>
2024-09-28 21:00:12 -0300

    Informações solicitadas pelo terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,10 +1,23 @@
+
+import java.util.Locale;
 import java.util.Scanner;
 
 // Classe que simula um terminal de caixa eletrônico para contas bancárias
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        
 
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor digite o numero da conta: ");
+        int numeroConta = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o numero da agencia: ");
+        String numeroAgenciaString = sc.nextLine();
+
+        System.out.println("Por favor informe o seu nome: ");
+        String nomeCliente = sc.nextLine();
+        System.out.println("Por favor informe a saldo da conta: ");
+        double saldo = sc.nextDouble();
+        System.out.println("\nOlá " + nomeCliente +" obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgenciaString + ", sua  conta é " + numeroConta + ", e seu saldo " + saldo + " já está disponível para saque");
     }
 }
