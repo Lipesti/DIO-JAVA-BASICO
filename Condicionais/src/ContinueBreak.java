4f5f08194f696aa4c901f4776cae3b4ee1f31266
Felipe Santos <lipes.santos02@gmail.com>
2024-10-01 13:56:28 -0300

    Exemplos com Laços de repetição

---
+++ Condicionais/src/ContinueBreak.java
@@ -0,0 +1,17 @@
+public class ContinueBreak {
+    public static void main(String[] args) {
+        
+        for(int numero=1; numero <= 5; numero++) {
+            if(numero == 3) {
+                break;
+
+            
+            }
+            
+            System.out.println(numero);
+
+            
+
+        }
+    }
+}
